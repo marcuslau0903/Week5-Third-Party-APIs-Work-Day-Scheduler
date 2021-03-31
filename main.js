205e0c85992ffec4620d7d4bd801f2e8c3c7e855
MarcusLau <marcuslau0903@gmail.com>
2021-03-31 21:33:05 +0100

    progress

--- main.js
+++ main.js
@@ -4,36 +4,46 @@ const renderCurrentDate = () => {
     dateTime.text(displayNow)
 }
 
+// function to add textarea data to local storage
 const renderCalenderEvents = () => {
-    //get from local storage
-    const plannerEvents = localStorage.getItem("plannerEvents")
-    if(plannerEvents !== null) {
+        //get from local storage
+    const plannerEvents = JSON.parse(localStorage.getItem("plannerEvents"))
+        // check if data is not null (false, do nothing)
+if(plannerEvents !== null) {
         // DECLARE A VARIABLE TO GET CURRENT HOUR -> 19
         // const currentHour = moment().hour()
-        const currentHour = 11
-        // get time block elements -> [9am_block, 10am_block,.....]
-        const timeBlocksArray = $(".container .row") 
 
+        const currentHour =  moment().hour()
+        // get time block elements array -> [9am_block, 10am_block,.....]
+        const timeBlocksArray = $(".container .row") 
+        // iterate over array
         const callback = function () {
+        //get time value from the row
+        const textArea =  $(this).find("textarea")
         const timeBlockTime = Number.parseInt($(this).data("time"), 10) 
-        if (timeBlockTime === currentHour) {
-            $(this).find("textarea").removeClass("past").addClass("present")
+        // check value time in time block against current hour
+    if (timeBlockTime === currentHour) {
+            // get child from container "this"
+            textArea.removeClass("past").addClass("present")
         }
-         if (timeBlockTime > currentHour) {
-            $(this).find("textarea").removeClass("past").addClass("future")
+    if (timeBlockTime > currentHour) {
+            textArea.removeClass("present").addClass("future")
         }
+
+        const plannedEvent = plannerEvents[timeBlockTime]
+        textArea.text(plannedEvent)
     }
-        timeBlocksArray.forEach(callback)
+        timeBlocksArray.each(callback)
     } else {
+        // add empty array in the local storage
         localStorage.setItem("plannerEvents", JSON.stringify({}));
     }
-  
 }
 
 const onReady = () => {
     renderCurrentDate()
     renderCalenderEvents()
 }
-
+//add function to display when window is loading
 $(document).ready(onReady)
 
